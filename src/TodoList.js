93f95c0ad1ccce20fc3e72d24ffdf41ae9c3159e
Alberto Alcantara <jalcantara@vsys.com>
2021-10-12 12:46:54 -0500

    Mi primer commit

---
+++ src/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './TodoList.css'
+function TodoList(props){
+    
+
+    return(
+
+        <section>
+
+
+            {props.children}
+        </section>
+
+    );
+
+}
+
+
+export {TodoList};
